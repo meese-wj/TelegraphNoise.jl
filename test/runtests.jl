7618e454b096196a07a62aec62b737b93ad0939f
W. Joe Meese <meese022@umn.edu>
2022-06-13 16:14:12 -0500

    Developing the package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
 using TelegraphNoise
 using Test
+using Random
+
+Random.seed!(42) # For tesing purposes.
 
 @testset "TelegraphNoise.jl" begin
-    # Write your tests here.
+    # Test the expected autocorrelation time
+    @test let 
+        tele = Telegraph(50.0, [0.])
+        expd_τ(tele) == 25.0
+    end
+
+    # Test the (dwell_time, signal_length) constructor
+    @test let 
+        tele = Telegraph(2.0, 10)
+        ( tele.dwell_time == 2.0 &&
+          tele.signal == [1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0] )
+    end
 end
